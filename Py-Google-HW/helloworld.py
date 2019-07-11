67f509057f05b4613fcc012b5adb2ff57ffb15b2
SPValli <52734902+SPValli@users.noreply.github.com>
2019-07-11 15:32:20 +0530

    Update helloworld.py

--- Py-Google-HW/helloworld.py
+++ Py-Google-HW/helloworld.py
@@ -1,5 +1,5 @@
 
-if __name__ == '__main__':
+if __value__ == '__main__':
     print('Hello Valli- Jul 11 - 10:30 am')
     print ('this is branch 1.1.')
     print ('this is a new change in branch 1.1')
